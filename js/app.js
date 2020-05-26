9e3ea9cdbb4b62c4051aece152d0c538c8092614
RandomSavage <50834695+RandomSavage@users.noreply.github.com>
2020-05-26 12:43:29 -0400

    working on click away option for mobile-menu to close without having to click button again if user so wishes

--- js/app.js
+++ js/app.js
@@ -2,9 +2,13 @@
 const myFunction = ()=> {
   let menuBtn = document.querySelector('.menu-btn');
   let mobileMenu = document.querySelector('.mobile-menu');
+  let querpo = document.getElementById('landing-section');
   if(mobileMenu.style.display = "none") {
     mobileMenu.classList.toggle('active');
     mobileMenu.style.display = "flex";
+    querpo.addEventListener('click', function() {
+      mobileMenu.style.display = "none";
+    })
   } else {
     console.log("active");
   }
